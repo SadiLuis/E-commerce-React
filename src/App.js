b0f5fac9e4a53e711cd33c171359bdb717002278
SadiLuis <euumemo2013@gmail.com>
2022-04-25 09:26:25 -0300

    algunos cambios en la ruta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 import Detail from "./Screens/Details/Details";
 import Home from "./Screens/Home/Home";
 
@@ -13,8 +14,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
         <Route path="/detail/:idProduct" element={<Detail />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
 
     </div>
