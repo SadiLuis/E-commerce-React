f5e6a034cf19bd186bb76ea740abc7768bbe2ec0
unknown <egil90@hotmail.com>
2022-04-28 21:45:10 -0500

    landing, mejoras en estilos

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import Detail from "./Screens/Details/Details";
 import Home from "./Screens/Home/Home";
-
-import  NavBarAll from "./Components/NavBar/NavBar"
+import Landing from './Screens/Landing/Landing'
+import NavBarAll from "./Components/NavBar/NavBar"
 import ContactForm from "./Components/ContactForm/ContactForm";
 import Profile from "./Screens/Profile/Profile"
 
@@ -20,22 +20,23 @@ import Cart from './Components/Cart/Cart'
 function App() {
   return (
     <div className="App">
-       
-        <NavBarAll /> 
-         
+
+      <NavBarAll />
+
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/detail/:idProduct" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/contactform" element={<ContactForm />} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/profile" element={<Profile />} />
         <Route path='/cart' element={<Cart />} />
+        <Route exact path="/" element={<Landing />} />
         {/* <Route path="/user" element={<Profile/>}/> */}
 
 
       </Routes>
-      
+
 
     </div>
   );
