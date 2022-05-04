6e92d2688b48321becadbcf5bfde7341e664e2b4
LucMuno <lucianommunoz@gmail.com>
2022-05-04 20:57:28 -0300

    Componente CreateProduct

--- src/Components/AdminProduct/validation.js
+++ src/Components/AdminProduct/validation.js
@@ -1,27 +1,27 @@
 export function validation(input){
     let errors ={};
 
-    if(!input.title){
-        errors.title ="enter the title please"
+    if(!input.title.trim()){
+        errors.title ="ingrese el nombre por favor"
     }
-    if(!input.price){
-        errors.price ="enter the price please"
+    if(!input.price.trim()){
+        errors.price ="ingrese el precio por favor"
     }else if(input.price < 0){
-        errors.price ="The value cannot be negative"
+        errors.price ="el valor no puede ser negativo"
     }
-    if(!input.description){
-        errors.description ="enter the description please"
+    if(!input.description.trim()){
+        errors.description ="ingrese la descripción por favor"
     }
     if(!input.images){
-        errors.images ="enter the images please"
+        errors.images ="ingrese imagen/es por favor"
     }
-    if(!input.cantidad){
-        errors.stock ="enter the stock please"
+    if(!input.cantidad.trim()){
+        errors.cantidad ="ingrese el stock por favor"
     }else if(input.cantidad < 0){
-        errors.price ="The value cannot be negative"
+        errors.price ="el stock no puede ser negativo"
     }
-    if(!input.category){
-        errors.category ="enter the categories please"
+    if(!input.categoriaId.trim()){
+        errors.categoriaId ="seleccione categoría por favor"
     }
    
     return errors;
