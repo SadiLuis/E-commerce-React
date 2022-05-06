e0b41ffc3e48e50e17bfa67b4d86bc5a30339504
unknown <egil90@hotmail.com>
2022-05-06 00:27:18 -0500

    avance Newsletter, mejoras footer y estilos

--- src/Screens/Landing/Landing.jsx
+++ src/Screens/Landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import uno from '../../Assets/uno.JPG'
 import dos from '../../Assets/dos.JPG'
 import tres from '../../Assets/tres.JPG';
@@ -317,18 +317,18 @@ const Landing = () => {
                       </strong>
                     </div>
                   </div>
-                  <div class="links">
+                  <div>
                     <div class="mb-5">
-                      <div class="link">
-                        <div class="m-0">
-
-                          <span>
+                      <div>
+                        <div class="m-0" >
+                          <span class="links" onClick={() => navigate('/about')}>
                             About Us
                           </span>
 
                         </div>
                       </div>
                     </div>
+
                   </div>
                 </div>
               </div>
@@ -348,15 +348,13 @@ const Landing = () => {
                       </strong>
                     </div>
                   </div>
-                  <div class="links">
+                  <div>
                     <div class="mb-5">
                       <div class="link">
                         <div class="m-0">
-
-                          <span onClick={() => navigate('/contactform')}>
+                          <span class="links" onClick={() => navigate('/contactform')}>
                             E-mail Us
                           </span>
-
                         </div>
                       </div>
                     </div>
@@ -379,15 +377,13 @@ const Landing = () => {
                       </strong>
                     </div>
                   </div>
-                  <div class="links">
+                  <div>
                     <div class="mb-5">
                       <div class="link">
                         <div class="m-0">
-
-                          <span>
-                            FAQ
+                          <span class="links" onClick={() => navigate('/faqs')}>
+                            FAQs
                           </span>
-
                         </div>
                       </div>
                     </div>
@@ -395,13 +391,8 @@ const Landing = () => {
                 </div>
               </div>
             </div>
-
-
-
           </div>
-
         </div>
-
       </div>
     </div >
   )
