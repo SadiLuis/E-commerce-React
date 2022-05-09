973508fb41dd5f2a739f75e35d43dc8c3619bd51
Robledo Claudio <claudio.n.robledo@gmail.com>
2022-05-09 19:20:21 -0300

    fix bugs

--- src/Components/AdminProduct/EditProduct.jsx
+++ src/Components/AdminProduct/EditProduct.jsx
@@ -1,20 +1,21 @@
 import React, { createRef, useEffect, useState } from 'react'
 import { postProduct } from "../../Actions/products"
 import { getAllCategories } from "../../Actions/Category.js"
-import {getProductById, putProductByID} from "../../Actions/products.js"
+import { getProductById, putProductByID } from "../../Actions/products.js"
 import { useSelector, useDispatch } from "react-redux";
 import './EditProduct.css'
 import Axios from "axios";
 import icon from '../../Assets/pencil.svg'
 import { validation } from "./validation";
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2'
 import { useParams } from 'react-router-dom';
+import SidebarAdmin from '../SidebarAdmin/SidebarAdmin';
 
 
 
 export default function Product() {
-  const { idProduct } = useParams();  
+  const { idProduct } = useParams();
   const dispatch = useDispatch()
   let myRef = createRef()
   const categories = useSelector((state) => state.categoriesReducer.categories);
@@ -22,7 +23,8 @@ export default function Product() {
   const [inputImages, setInputImages] = useState("");
   let [index, setIndex] = useState(0)
   var data = {}
-  console.log("producto", product)
+  //console.log("producto", product)
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getProductById(idProduct))
@@ -49,8 +51,8 @@ export default function Product() {
     size: product.size,
     cantidad: product.cantidad,
   });
-  
-  const [focus , setFocus] = useState({ 
+
+  const [focus, setFocus] = useState({
     title: false,
     price: false,
     description: false,
@@ -64,51 +66,52 @@ export default function Product() {
     width: [],
     depth: [],
   });
-  
-  console.log("input", input)
+
+  //console.log("input", input)
   const [error, setError] = useState({})
 
-  
+
   function handleChangeSize(e) {
     setInputsize({
       ...inputsize,
       [e.target.name]: e.target.value,
     });
   }
-  if(!input.size){
-  input.size = (inputsize.height + "x").concat((inputsize.width) + "x").concat(inputsize.depth) + "cm"
+  if (!input.size) {
+    input.size = (inputsize.height + "x").concat((inputsize.width) + "x").concat(inputsize.depth) + "cm"
   }
   useEffect(() => {
     dispatch(getAllCategories())
   }, [])
-   
-  
+
+
   //data.title = input.title;
   //data.price = input.price;
-  
+
   const handleSubmit = (e) => {
     e.preventDefault()
-       
-    if(Object.keys(error).length){
-      
+
+    if (Object.keys(error).length) {
+
       Swal.fire({
         text: `Datos incorrectos , por favor verifique que los datos ingresados sean correctos`,
         icon: "error",
         confirmButtonText: "Ok",
       });
     }
-    else{
-    setInput(input)
-    dispatch(putProductByID(idProduct, input))
-    Swal.fire({
-      text: `Producto atualizado con éxito!`,
-      icon: "success",
-      confirmButtonText: "Ok",
-    });
+    else {
+      setInput(input)
+      dispatch(putProductByID(idProduct, input))
+      Swal.fire({
+        text: `Producto atualizado con éxito!`,
+        icon: "success",
+        confirmButtonText: "Ok",
+      });
+      navigate('/dashboard/admin')
     }
 
   }
-   
+
 
   //console.log("input", input.price)
   const handleTab = (index) => {
@@ -120,22 +123,22 @@ export default function Product() {
     images[index].className = "active"
   }
 
-   const handleChange = (name, value) => {
-    console.log(input)
+  const handleChange = (name, value) => {
+    //console.log(input)
     const newform = { ...input, [name]: value };
-    if(typeof value === 'string' || typeof value === 'array'){
-    setInput(newform);
-    //const errors = validation(newform);
-    //console.log("error", errors)
-    //setError(errors);
-    } 
-    else{
-    setFocus({ ...focus, [name]: value })
+    if (typeof value === 'string' || typeof value === 'array') {
+      setInput(newform);
+      //const errors = validation(newform);
+      //console.log("error", errors)
+      //setError(errors);
+    }
+    else {
+      setFocus({ ...focus, [name]: value })
     }
     return newform;
   }
   function handleSelectCategory(e) {
-    console.log(input)
+    //console.log(input)
 
     setInput({
       ...input,
@@ -143,7 +146,7 @@ export default function Product() {
     });
   }
   function addImage(e) {
-    
+
     setInput({
       ...input,
       images: [...input.images, { url: inputImages, alt: "" }],
@@ -157,7 +160,7 @@ export default function Product() {
     for (let i = 0; i < files.length; i++) {
       formData.append("file", files[i]);
       formData.append("upload_preset", 'preset_pabs');
-      console.log("img", formData)
+      //console.log("img", formData)
       const newAxios = Axios.create();
       newAxios
         .post(
@@ -182,185 +185,194 @@ export default function Product() {
   }
 
   return (
-    <div className='container-md'>
-      <form
-       onSubmit={handleSubmit}
-        >
-        <h2 className="titulo">Editar Producto</h2>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">Nombre</span>
-          <input onFocus={(e) => handleChange(e.target.name , true)}
-            type="text"
-            class="form-control"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            name='title'
-            value={input.title}
-            onChange={(e) => handleChange(e.target.name, e.target.value)}> 
-          </input>
-          {focus.title && error.title && <span>{error.title}</span> }
-        </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">Categoría</span>
-          <span class="form-control">{input.categoriaId}</span>
-          <select class="form-select" aria-label="Default select example" name='categoria' 
-          onFocus={(e) => handleChange(e.target.name , true)}
-          onChange={(e) => handleSelectCategory(e)}>
-            <option hidden selected>Modificar categoría</option>
-            {categories?.map((category) => (
-              <option value={category.id}>{category.nombre}</option>
-            ))}
-          </select>
-          {focus.categoriaId && error.categoriaId && <span>{error.categoriaId}</span> }
-        </div>
-        <div class="input-group">
-          <span class="input-group-text">Descripción</span>
-          <textarea onFocus={(e) => handleChange(e.target.name , true)}
-            class="form-control"
-            aria-label="With textarea"
-            name='description'
-            value={input.description}
-            onChange={(e) => handleChange(e.target.name, e.target.value)}
-          ></textarea>
-          {focus.description && error.description && <span>{error.description}</span> }
-        </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">Medidas</span>
-          <input onFocus={(e) => handleChange(e.target.name , true)}
-            type="text"
-            class="form-control"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            name='size'
-            value={input.size}
-            onChange={(e) => handleChange(e.target.name, e.target.value)}> 
-          </input>
-          <span class="input-group-text" id="basic-addon1">Alto</span>
-          <input type="number"
-            class="form-control"
-            placeholder="cm"
-            name='height'
-            value={inputsize.height}
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            onChange={(e) => handleChangeSize(e)}>
-          </input>
-          <span class="input-group-text" id="basic-addon1">Ancho</span>
-          <input type="number"
-            class="form-control"
-            placeholder="cm"
-            name='width'
-            value={inputsize.width}
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            onChange={(e) => handleChangeSize(e)}>
-          </input>
-          <span class="input-group-text" id="basic-addon1">Profundidad</span>
-          <input type="number"
-            class="form-control"
-            placeholder="cm"
-            name='depth'
-            value={inputsize.depth}
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            onChange={(e) => handleChangeSize(e)}>
-          </input>
-        </div>
-        <div className="thumb" ref={myRef}>
-          {input.images?.map((img, index) => (
-            <img src={img} alt="product" key={index}
-              onClick={() => handleTab(index)}
-            />
-          ))}
-        </div>
-        <div>
-          <label>Imagenes</label>
-          <div>
-            <input
-              type="text"
-              placeholder="URL..."
-              value={inputImages}
-              onChange={(e) => setInputImages(e.target.value)}
-            />
-            <img
-              className="cursor-pointer"
-              onClick={(e) => addImage(e)}
-
-              alt=""
-            />
-          </div>
-          <div>
-            <input
-              type="file"
-              multiple
-              onChange={(e) => {
-                uploadImage(e.target.files);
-              }}
-            ></input>
-          </div>
-          <div className="flex">
-            {input.images &&
-              input.images.flat().map((name) => {
-                return (
-                  <div>
-                    <img
-
-                      src={name.url}
-                      alt={name.url}
-                    />
-                    <button
-
-                      name={name}
-                      onClick={(name) => handleDeleteImage(name)}
-                    >
-                      X
-                    </button>
-                  </div>
-                );
-              })}
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">Precio</span>
-          <input onFocus={(e) => handleChange(e.target.name , true)} 
-            type="text"
-            class="form-control"
-            placeholder="$ 0.00"
-            name='price'
-            value={input.price}
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            onChange={(e) => handleChange(e.target.name, e.target.value)}>
-          </input>
-          {focus.price && error.price && <span>{error.price}</span> }
+    <div className='container-fluid'>
+      <div className="row">
+        <div className="col-auto col-md-2 col-xl-2 px-0 ">
+          <SidebarAdmin />
         </div>
 
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">Stock</span>
-          <input onFocus={(e) => handleChange(e.target.name , true)}  
-            type="text"
-            class="form-control"
-            placeholder="ingrese stock"
-            name='cantidad'
-            value={input.cantidad}
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            onChange={(e) => handleChange(e.target.name, e.target.value)}>
-          </input>
-          {focus.cantidad && error.cantidad && <span>{error.cantidad}</span> }
-        </div>
-        <div>
-        <button
-          text="Create Product"
-          type="submit"
-          onClick={(e) => handleSubmit(e)}>Actualizar Producto
+        <div className="col-10">
+          <form
+            onSubmit={handleSubmit}
+          >
+            <h2 className="titulo">Editar Producto</h2>
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">Nombre</span>
+              <input onFocus={(e) => handleChange(e.target.name, true)}
+                type="text"
+                className="form-control"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                name='title'
+                value={input.title}
+                onChange={(e) => handleChange(e.target.name, e.target.value)}>
+              </input>
+              {focus.title && error.title && <span>{error.title}</span>}
+            </div>
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">Categoría</span>
+              <span className="form-control">{input.categoriaId}</span>
+              <select className="form-select" aria-label="Default select example" name='categoria'
+                onFocus={(e) => handleChange(e.target.name, true)}
+                onChange={(e) => handleSelectCategory(e)}>
+                <option hidden selected>Modificar categoría</option>
+                {categories?.map((category) => (
+                  <option value={category.id}>{category.nombre}</option>
+                ))}
+              </select>
+              {focus.categoriaId && error.categoriaId && <span>{error.categoriaId}</span>}
+            </div>
+            <div className="input-group">
+              <span className="input-group-text">Descripción</span>
+              <textarea onFocus={(e) => handleChange(e.target.name, true)}
+                className="form-control"
+                aria-label="With textarea"
+                name='description'
+                value={input.description}
+                onChange={(e) => handleChange(e.target.name, e.target.value)}
+              ></textarea>
+              {focus.description && error.description && <span>{error.description}</span>}
+            </div>
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">Medidas</span>
+              <input onFocus={(e) => handleChange(e.target.name, true)}
+                type="text"
+                className="form-control"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                name='size'
+                value={input.size}
+                onChange={(e) => handleChange(e.target.name, e.target.value)}>
+              </input>
+              <span className="input-group-text" id="basic-addon1">Alto</span>
+              <input type="number"
+                className="form-control"
+                placeholder="cm"
+                name='height'
+                value={inputsize.height}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                onChange={(e) => handleChangeSize(e)}>
+              </input>
+              <span className="input-group-text" id="basic-addon1">Ancho</span>
+              <input type="number"
+                className="form-control"
+                placeholder="cm"
+                name='width'
+                value={inputsize.width}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                onChange={(e) => handleChangeSize(e)}>
+              </input>
+              <span className="input-group-text" id="basic-addon1">Profundidad</span>
+              <input type="number"
+                className="form-control"
+                placeholder="cm"
+                name='depth'
+                value={inputsize.depth}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                onChange={(e) => handleChangeSize(e)}>
+              </input>
+            </div>
+            <div className="thumb" ref={myRef}>
+              {input.images?.map((img, index) => (
+                <img src={img} alt="product" key={index}
+                  onClick={() => handleTab(index)}
+                />
+              ))}
+            </div>
+            <div>
+              <label>Imagenes</label>
+              <div>
+                <input
+                  type="text"
+                  placeholder="URL..."
+                  value={inputImages}
+                  onChange={(e) => setInputImages(e.target.value)}
+                />
+                <img
+                  className="cursor-pointer"
+                  onClick={(e) => addImage(e)}
 
-        </button>
-        <Link to = {`/dashboard/admin`}> <button>Cancelar</button></Link>
+                  alt=""
+                />
+              </div>
+              <div>
+                <input
+                  type="file"
+                  multiple
+                  onChange={(e) => {
+                    uploadImage(e.target.files);
+                  }}
+                ></input>
+              </div>
+              <div className="flex">
+                {input.images &&
+                  input.images.flat().map((name) => {
+                    return (
+                      <div>
+                        <img
+
+                          src={name.url}
+                          alt={name.url}
+                        />
+                        <button
+
+                          name={name}
+                          onClick={(name) => handleDeleteImage(name)}
+                        >
+                          X
+                        </button>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">Precio</span>
+              <input onFocus={(e) => handleChange(e.target.name, true)}
+                type="text"
+                className="form-control"
+                placeholder="$ 0.00"
+                name='price'
+                value={input.price}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                onChange={(e) => handleChange(e.target.name, e.target.value)}>
+              </input>
+              {focus.price && error.price && <span>{error.price}</span>}
+            </div>
+
+            <div className="input-group mb-3">
+              <span className="input-group-text" id="basic-addon1">Stock</span>
+              <input onFocus={(e) => handleChange(e.target.name, true)}
+                type="text"
+                className="form-control"
+                placeholder="ingrese stock"
+                name='cantidad'
+                value={input.cantidad}
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                onChange={(e) => handleChange(e.target.name, e.target.value)}>
+              </input>
+              {focus.cantidad && error.cantidad && <span>{error.cantidad}</span>}
+            </div>
+            <div>
+              <button
+                text="Create Product"
+                type="submit"
+                onClick={(e) => handleSubmit(e)}>Actualizar Producto
+
+              </button>
+              <Link to={`/dashboard/admin`}> <button>Cancelar</button></Link>
+            </div>
+
+          </form>
         </div>
-        
-      </form>
-     
+      </div>
+
+
     </div>
   )
 
